df6abe9750164609c74cd7f60f069bc88f928241
tywando <tybof15@gmail.com>
2025-09-30 14:06:47 -0700

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,29 @@
+/* CS261- Recitation 1 - Q. 1*
+ * Name : YOUR Name
+ * Recitation time: TIME
+ * Solution description: check the properties, like value and address, of pointer
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+void myFuntion(int* iptr){
+     /*Print the value pointed to by iptr*/
+     
+     /*Print the address pointed to by iptr*/
+
+     /*Print the address of iptr itself*/
+}
+
+int main(){
+    
+    /*declare an integer x and initialize it with 0*/
+    
+    /*print the address of x*/
+    
+    /*Call myFunction() with the address of x*/
+    
+    /*print the value of x*/
+
+    return 0;
+}
---
+++ Q2.c
@@ -0,0 +1,40 @@
+/* CS261- Recitation 1 - Q.2
+ * Name:  YOUR NAME
+ * Recitation time:
+ * Complete the code below 
+*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(int a, int* b, int c[], int n){
+    /* Set a to double its original value*/
+	
+    /* Set b to the maximum value in the array c[] using a for loop */
+	
+    /* Set c[0] to triple its original value */
+
+
+    /* Return  the sum of a and b */
+
+}
+
+int main(){
+    int n = 5;  // the size of the array c
+    int c[5] = {4,9,1,2,0};
+
+    /*Declare two integers x and y and initialize them to 2 and 4 respectively*/
+	
+    
+    /*Print the values of x, y and the first element of c in one line*/
+
+    /*Call foo() appropriately, passing x,y,c,n as parameters*/
+    /*Print the value returned by foo*/
+
+    /*Print the values of x, y and the first element of c  again*/
+
+    /*  Did the values printed change? Why? */
+    return 0;
+}
+    
+    
